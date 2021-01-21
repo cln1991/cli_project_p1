1cb90e32674dcce2a2141a3dbd454e560dad0b94
Christopher <christophernavac@gmail.com>
2021-01-21 17:54:14 -0500

    updating my project

--- bin/yugis-card-shop.rb
+++ bin/yugis-card-shop.rb
@@ -3,6 +3,3 @@
 require './lib/yugis_card_shop'
 
 YugisCardShop::CLI.new.call
-
-#With setting up this file like this, we dont have to add anything more to this. 
-#We can now just work on our cli.rb which will be holding all the information, or at least most of it.
